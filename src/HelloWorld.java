07ec64fee56764f8a7d849fb6a1ef827fd32ff21
Narayan Bhattarai <nayaranbhattarai2003@gmail.com>
2026-04-16 21:51:12 +0530

    Update HelloWorld.java
    Refactored HelloWorld EV3 code:
    - Removed duplicate imports
    - Added missing LCD and Delay imports
    - Fixed method name and spelling errors
    - Improved formatting and readability

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,27 +1,29 @@
 import lejos.hardware.Button;
-import lejos.hardware.Button;
-import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
 
-public class Helloworld
-{
-    public static void main (String[] args)
-    {
+public class HelloWorld {
+    public static void main(String[] args) {
 
-     // this is my first LEGO code 
-     // make me autonomus
-     // press any button to stop
+        // This is my first LEGO code
+        // Make me autonomous
+        // Press any button to stop the program
+        // Narayan Bhattarai
 
-     LCD.clear();
-     LCD.drawString("Wellcome", 0, 0);
-     Delay.msDelay(1000);
+        LCD.clear();
+        LCD.drawString("Welcome", 0, 0);
+        Delay.msDelay(1000);
 
-     LCD.drawString("This is my first LEGO code.", 0, 1);
-     Delay.msDelay(2000);
-     LCD.drawString("Make me autonomus", 0, 2);
-     LCD.drawString("Press any button to stop.", 0, 3);
+        LCD.drawString("This is my first", 0, 1);
+        LCD.drawString("LEGO code.", 0, 2);
+        Delay.msDelay(2000);
 
-     // wait to a button press to exist
-     Button.waitForAnyprss();
+        LCD.clear();
+        LCD.drawString("Make me autonomous", 0, 0);
+        LCD.drawString("Press any button", 0, 2);
+        LCD.drawString("to stop.", 0, 3);
 
-    }     
+        // Wait for a button press to exit
+        Button.waitForAnyPress();
+    }
 }
